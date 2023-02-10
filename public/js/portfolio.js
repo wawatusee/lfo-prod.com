5c214b2a5e6ba4a8c050916c62cc870eb060b78a
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-02-10 19:27:06 +0100

    portfolio.js Player et showcase fonctionnel

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,3 +1,7 @@
 function play_this_video(filePath){
-    console.log(filePath);
+    var playerVideo=document.getElementById('playervideo');
+    var sourceVideo=playerVideo.getElementsByTagName("source")[0];
+    sourceVideo.src=filePath;
+    console.log(sourceVideo.src);
+    playerVideo.load();
 }
