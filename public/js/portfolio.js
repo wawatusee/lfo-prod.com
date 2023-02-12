d7fe4f0150d4d0b63135b9bfdc7e11df503230fd
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-02-12 12:15:03 +0100

    Titre de chaque thumb-video en majuscules

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,6 +1,6 @@
+const playerVideo=document.getElementById('playervideo');
+const sourceVideo=playerVideo.getElementsByTagName("source")[0];
 function play_this_video(filePath){
-    var playerVideo=document.getElementById('playervideo');
-    var sourceVideo=playerVideo.getElementsByTagName("source")[0];
     sourceVideo.src=filePath;
     console.log(sourceVideo.src);
     playerVideo.load();
