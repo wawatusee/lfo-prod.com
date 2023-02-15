c1844c509d8e8c9bf614131c91576a8a43a3f3ce
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-02-15 12:05:13 +0100

    header.php, poster background video

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <header class="header">
-   <video id="background-video" src="/public/videos/mariamaentete.mp4" autoplay loop muted poster="https://assets.codepen.io/6093409/river.jpg"></video>
+   <video id="background-video" src="/public/videos/mariamaentete.mp4" autoplay loop muted poster="/public/img/backgroundheader-mariama-fix.png"></video>
    <div class="bantitre">
       <h1><?= $titre[0]?></h1>
       <div class="soustitre"><?= $titre[1]?></div>
