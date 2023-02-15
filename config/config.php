734282ab665e996955b3aeebeae4cabbbcc23fc7
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-02-15 10:44:17 +0100

    style.css, header height background video

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php require_once("../model/menus_model.php");
-  $titre=["LFOPROD","FILM-CORPORATE-COMMERCIAL","logo.png"];
+  $titre=["LFO","FILM-CORPORATE-COMMERCIAL","logo.png"];
 $menus=new MenusModel("../json/menus.json");
 $menuRS=$menus->getMenu("RS_menu");
