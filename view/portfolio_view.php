00e0d7600a651949a4534fe3f5c3a825a504cf5e
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-03-04 16:39:00 +0100

    style.css, titres videos, sous titre header @media

--- view/portfolio_view.php
+++ view/portfolio_view.php
@@ -7,8 +7,8 @@
         foreach($catalogArray as $product){
             if($product->visible){
             $this->portfolioView.='<a href="#portfolio" class="video-link" onClick="play_this_video(\''.$this->pathVideo.$product->file.'\')">';
-            $this->portfolioView.=$product->title;
             $this->portfolioView.='<div class="thumb-video" style="background-image:url(\''.$this->pathPoster.$product->poster.'\');">';
+            $this->portfolioView.='<h4>'.$product->title.'</h4>';
             $this->portfolioView.='<img class="thumb-video-arrow" src="img/arrow-play-video.svg" alt="Capture de la video">';
             $this->portfolioView.='</div>';
             $this->portfolioView.='</a>';
