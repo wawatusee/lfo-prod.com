035a8d4800414b4b271ede0932a6bf743c01add0
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-03-07 11:44:47 +0100

    main.php Déplacement section portfolio, style.css, hauteur thumb-video

--- inc/main.php
+++ inc/main.php
@@ -1,6 +1,19 @@
 <main>
-<section id="services" class="services">
-    <h2>OUR SERVICES</h2>
+    <section id="portfolio">
+        <h2>PORTFOLIO</h2>
+            <video id="playervideo" controls width="640" height="480" poster="img/content/thumbs-videos/mariamateaser.jpg" autoplay muted>
+                <source src="videos/MARIAMA-TEASER-FINAL-FINAL-1.2.mp4" type="video/mp4">
+            </video>
+            <?php require_once("../model/portfolio_model.php");
+            $portfolioData=new PortfolioModel("../json/portfolio.json");
+            $contentPortfolio=$portfolioData->getPortfolio();
+            require_once("../view/portfolio_view.php");
+            $portfolioView=new PortfolioView;
+            echo $portfolioView->getView($contentPortfolio->showcase);
+            ?>
+    </section>
+    <section id="services" class="services">
+        <h2>OUR SERVICES</h2>
         <p>Commercial and corporate films, Musical video clips, documentaries, fiction, motion and sound design, Testimonial, photography, media consulting and more... Film production has no secret for us.If you have a wish, we have the solutions to make it true. From an idea to a film. Our team and partners directed all the films you'll find on this website.</p>
         <div class="cards">
             <article class="card">
@@ -32,24 +45,11 @@
             </article>
         </div>
     </section>
-    <section id="portfolio">
-    <h2>PORTFOLIO</h2>
-        <video id="playervideo" controls width="640" height="480" poster="img/content/thumbs-videos/mariamateaser.jpg" autoplay muted>
-            <source src="videos/MARIAMA-TEASER-FINAL-FINAL-1.2.mp4" type="video/mp4">
-        </video>
-        <?php require_once("../model/portfolio_model.php");
-        $portfolioData=new PortfolioModel("../json/portfolio.json");
-        $contentPortfolio=$portfolioData->getPortfolio();
-        require_once("../view/portfolio_view.php");
-        $portfolioView=new PortfolioView;
-        echo $portfolioView->getView($contentPortfolio->showcase);
-        ?>
-    </section>
     <section id="contacts">
         <h2>CONTACTS</h2>
         <div class="contacts">
             <div class="presentation">
-                <a href="+32 473 18 46 80"><img src="/public/img/picto-phone.svg" alt="picto-téléphone"></a><span>+32 0473 18 46 80</span>
+                <a href="tel:+32473184680"><img src="/public/img/picto-phone.svg" alt="picto-téléphone"></a><span>+32 0473 18 46 80</span>
              </div>
             <div class="presentation">
                 <a href="mailto:contact@lfo-prod.com"><img src="/public/img/picto-mail.svg" alt="picto-mail"></a><span>contact@lfo-prod.com</span>
