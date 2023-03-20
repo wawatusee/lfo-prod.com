7d33bdb3c7a515295a9a9bba679205bd6a1ac7f6
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-03-20 12:08:38 +0100

    Google search ajouté

--- inc/head.php
+++ inc/head.php
@@ -5,4 +5,5 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <title><?= $titre[0]?></title>
+    <meta name="google-site-verification" content="QqqriLWjPHaBNuucfOuYUKnDUUZWLZFPXpMfceDDdyM" />
 </head>
